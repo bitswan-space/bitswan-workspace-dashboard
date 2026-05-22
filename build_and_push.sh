da22fa8b9bdfec3ec154a6a22766a8e64fa611af
Tomáš Peroutka <67205912+Pero-coder@users.noreply.github.com>
2026-05-22 14:49:01 +0200

    Enhance Docker image build and push script to include production image tagging and pushing. Added support for tagging and pushing the production Docker image alongside the staging image, improving deployment capabilities.

--- build_and_push.sh
+++ build_and_push.sh
@@ -4,14 +4,21 @@
 YEAR=$(date +%Y)
 COMMIT_HASH=$(git rev-parse --short HEAD)
 IMAGE_TAG="bitswan/workspace-dashboard-staging"
+IMAGE_TAG_PROD="bitswan/workspace-dashboard"
 
 # Build and push Docker images
-docker build -t $IMAGE_TAG:latest -t $IMAGE_TAG:$YEAR-${GITHUB_RUN_ID}-git-$COMMIT_HASH -f ./Dockerfile .
+docker build -t $IMAGE_TAG:latest -t $IMAGE_TAG:$YEAR-${GITHUB_RUN_ID}-git-$COMMIT_HASH -t $IMAGE_TAG_PROD:latest -t $IMAGE_TAG_PROD:$YEAR-${GITHUB_RUN_ID}-git-$COMMIT_HASH -f ./Dockerfile .
 
 docker push $IMAGE_TAG:latest
 docker push $IMAGE_TAG:$YEAR-${GITHUB_RUN_ID}-git-$COMMIT_HASH
+
+docker push $IMAGE_TAG_PROD:latest
+docker push $IMAGE_TAG_PROD:$YEAR-${GITHUB_RUN_ID}-git-$COMMIT_HASH
 
 # Push a tag with the image ID
 IMAGE_ID=$(docker images --no-trunc -q $IMAGE_TAG:latest | sed 's/:/_/g')
 docker tag $IMAGE_TAG:latest $IMAGE_TAG:$IMAGE_ID
 docker push $IMAGE_TAG:$IMAGE_ID
+
+docker tag $IMAGE_TAG_PROD:latest $IMAGE_TAG_PROD:$IMAGE_ID
+docker push $IMAGE_TAG_PROD:$IMAGE_ID
